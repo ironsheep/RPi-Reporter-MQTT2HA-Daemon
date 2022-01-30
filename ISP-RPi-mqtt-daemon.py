ebd550234ae44f6522f9862689f0561445ee1516
mcarlosro <mcarlosro@gmail.com>
2022-01-30 08:52:48 +0100

    Add network transfer information

--- ISP-RPi-mqtt-daemon.py
+++ ISP-RPi-mqtt-daemon.py
@@ -1,7 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 import _thread
 from datetime import datetime, timedelta
 from tzlocal import get_localzone
